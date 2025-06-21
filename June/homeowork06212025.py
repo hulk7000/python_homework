d1031c46331672cc60aa0c3dcbc8065180fc13ad
hulk7000 <hulkzhang7@gmail.com>
2025-06-21 10:50:10 -0700

    new

---
+++ June/homeowork06212025.py
@@ -0,0 +1,19 @@
+def avglist(lst):
+    try:
+        total = 0
+        count = 0
+        length = len(lst)
+        for i in lst:
+            total += int(i)
+            count += 1
+        avg = total / count
+        return avg
+    except(ValueError, TypeError) as e:
+        return f'your list not valid\n{e}'
+
+
+listnum=[87, 23, 14, 59, 44, 91, 89, 35, 11, 62, 48, 8, 20, 73, 5, 38, 84, 31, 16, 90, 55, 26, 64, 12, 79, 6]
+output = avglist(listnum)
+print(output)
+
+#10
