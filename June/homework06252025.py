7cef20e3fb075860fc7b6dcf73218f1bf8338f0f
hulk7000 <hulkzhang7@gmail.com>
2025-06-26 22:22:05 -0700

    game1 upgrade while and input validation

--- June/homework06252025.py
+++ June/homework06252025.py
@@ -1,28 +1,35 @@
-import random
+def game1():
+    import random
+    choices = ['rock', 'paper', 'scissor']
+    user_Score = 0
+    computer_Score = 0
+    while user_Score <2 and computer_Score <2:
+        while True:
+            user = input("Choose rock, paper, or scissor: ").lower()
+            if user in choices:
+                break
+            print("Invalid choice. Please choose only 'rock', 'paper', or 'scissor'.")
+        computer = random.choice(choices)
+        print(f'computer chose {computer}')
 
-choices = ['rock', 'paper', 'scissor']
-user_Score = 0
-computer_Score = 0
+        if user == computer:
+            print('Draw this round!')
+            pass
+        elif (user == 'rock' and computer == 'scissor') or \
+            (user == 'paper' and computer == 'rock') or \
+            (user == 'scissor' and computer == 'paper'):
+            user_Score += 1
+            print('User wins this round!')
+        else:
+            computer_Score += 1
+            print('Computer wins this round!')
 
-for _ in range(3):
-    user = input('choose rock, paper, or scissor: ').lower()
-    computer = random.choice(choices)
-    print(f'Computer chose {computer}')
-
-    if user == computer:
-        pass
-    elif (user == 'rock' and computer == 'scissor') or \
-        (user == 'paper' and computer == 'rock') or \
-        (user == 'scissor' and computer == 'paper'):
-        user_Score += 1
+        print(f'your score is {user_Score} , computer score is {computer_Score}')
+    if user_Score > computer_Score:
+        print('You won!')
+    elif user_Score < computer_Score:
+        print('You lost!')
     else:
-        computer_Score += 1
-
-if user_Score > computer_Score:
-    print('You won!')
-elif computer_Score < user_Score:
-    print('You lost!')
-else:
-    print('Draw!')
-
-#10
+        print('Draw!')
+game1()
+#9
