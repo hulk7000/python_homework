f891b79fe6395045d067d95f8bce388bb600489f
Hulk Zhang <hulk7000@proton.me>
2025-07-13 13:45:25 -0700

    calculater

--- June/cal.py
+++ June/cal.py
@@ -1,26 +1,26 @@
 def cal():
     numbers = []
-    functions = ['+', '-', '*', '/']
+    functions = ['+' , '-', '*', '/']
     while True:
         while True:
-            a_input = input('Number 1: ')
+            a_input = input('Number1: ')
             try:
                 a = int(a_input)
                 break
             except ValueError:
-                print('Invalid input. Please enter a valid number.')
+                print('Invalid input')
         while True:
             func = input('+ - * /: ')
             if func in functions:
                 break
-            print('Invalid input.')
+            print('Invalid input')
         while True:
-            b_input = input('Number 2: ')
+            b_input = input('Number2: ')
             try:
                 b = int(b_input)
                 break
             except ValueError:
-                print('Invalid input. Please enter a valid number.')
+                print('Invalid input')
         break
     try:
         if func == "+":
@@ -36,3 +36,4 @@ def cal():
         return e
 
 print(cal())
+#10
