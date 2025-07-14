a338716b271bacc001e8e639856252a61fe7086d
Hulk Zhang <hulk7000@proton.me>
2025-07-13 22:50:31 -0700

    db practice

---
+++ June/qytang4/db_practice.py
@@ -0,0 +1,15 @@
+import sqlite3
+conn = sqlite3.connect('qytangdb1.sqlite')
+cursor = conn.cursor()
+# cursor.execute('create table test(t1 int, t2 varchar(40))')
+# cursor.execute("insert into test(t1,t2) values(11, 'welcome to qytang')")
+# cursor.execute("insert into test(t1,t2) values(12, 'welcome to python')")
+# cursor.execute("insert into test(t1,t2) values(13, 'welcome to FRanK')")
+cursor.execute("insert into test(t1,t2) values(14, 'welcome to qytang')")
+cursor.execute("insert into test(t1,t2) values(15, 'welcome to python')")
+cursor.execute("insert into test(t1,t2) values(16, 'welcome to FRanK')")
+cursor.execute("select * from test")
+yourresult = cursor.fetchall()
+for i in yourresult:
+    print(i)
+conn.commit()
