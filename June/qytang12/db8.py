c533c0486172242aea6d38676e543f4c38bb2233
Hulk Zhang <hulk7000@proton.me>
2025-07-14 15:32:07 -0700

    sqlite practice

--- June/qytang12/db8.py
+++ June/qytang12/db8.py
@@ -1,10 +1,7 @@
 import sqlite3
 conn = sqlite3.connect('qytangdb1.sqlite')
 cursor = conn.cursor()
-# cursor.execute('create table test(t1 int, t2 varchar(40))')
-# cursor.execute("insert into test(t1,t2) values(11, 'welcome to qytang')")
-# cursor.execute("insert into test(t1,t2) values(12, 'welcome to python')")
-# cursor.execute("insert into test(t1,t2) values(13, 'welcome to FRanK')")
+cursor.execute('create table test(t1 int, t2 varchar(40))')
 cursor.execute("insert into test(t1,t2) values(11, 'welcome to qytang')")
 cursor.execute("insert into test(t1,t2) values(12, 'welcome to python')")
 cursor.execute("insert into test(t1,t2) values(13, 'welcome to FRanK')")
