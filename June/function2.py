5b791a237fb4ab78b133605d3c1ec6f522e4866f
Hulk Zhang <hulk7000@proton.me>
2025-07-31 09:51:36 -0700

    ip menu

--- June/function2.py
+++ June/function2.py
@@ -1,4 +1,6 @@
 import re
+from itertools import count
+from unittest import removeResult
 
 show = """vlan10   192.168.189.254     YES      CONFIG    up
 vlan20   172.16.1.2     YES      CONFIG    down
@@ -20,7 +22,6 @@ vlan10   155.168.5.254     YES      CONFIG    down
 
 re_all_list = re.findall(r'(vlan\d+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+YES\s+CONFIG\s+(up|down)\s+',show)
 
-
 def is_valid_vlan():
     pattern = r'^vlan(?:[1-9]|[1-9]\d{1,2}|[1-3]\d{3}|40[0-8][0-9]|409[0-6])$'
     while True:
@@ -29,54 +30,55 @@ def is_valid_vlan():
             break
     return vlan
 # print(is_valid_vlan())
+
 def is_valid_ip():
     ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
     while True:
         user_ip = input("Enter an IP to check: ").strip()
         if not ip_pattern.match(user_ip):
-            print("Invalid format. Please enter a valid IPv4 address (e.g., 192.168.1.2).")
+            print("Invalid IP")
             continue
         octets = user_ip.split('.')
         if all(0 <= int(o) <= 255 for o in octets):
             valid = 1
             break
         else:
-            print("Each octet must be between 0 and 255. Try again.")
+            print('Each octet must be between 0 and 255. Try again.')
             continue
     return user_ip
 # print(is_valid_ip())
 
 def checkstatus():
-    user_ip = is_valid_ip()     #5
+    user_ip = is_valid_ip()
     if user_ip in re_all_list:
         print(re_all_list[2])
     ipin = 0
     for i in re_all_list:
-        if user_ip == i[1]:
+        if user_ip == i[0]:
             print(i[2])
-            ipin=1
-    if ipin == 0:
-        print("No IP found")
+            ipin = 1
+        if ipin == 0:
+            print("No IP found")
 # checkstatus()
 
 def showallinfo():
-    template = "VLAN: {:>8}   IP: {:>15}   Status: {:>8}"
+    template = 'VLAN: {:>8} IP: {:>15} Status: {:>8}'
     for i in re_all_list:
         formatted = template.format(i[0], i[1], i[2])
         print(formatted)
 # showallinfo()
 
-def checkip():   #3
+def checkip():
     ipin = 0
     user_ip = is_valid_ip()
     for i in re_all_list:
-        if user_ip == i[1]:
-            print(f"VLAN : {i[0]} , IP : {i[1]} , status : {i[2]}")
+        if user_ip == i[0]:
+            print(f'VLAN : {i[0]} , IP: {i[1]} , Status: {i[2]}')
             ipin = 1
             break
-    if ipin==0:
-        print("IP address not found.")
-# checkip()
+    if ipin == 0:
+        print("No IP found")
+#checkip()
 
 def showvlan():
     vlan = is_valid_vlan()
@@ -84,16 +86,16 @@ def showvlan():
     for item in re_all_list:
         if item[0] == vlan:
             count += 1
-            print(f'{item[0]} IP : {item[1]} , status : {item[2]}')
+            print(f'{item[0]} IP: {item[1]} Status: {item[2]}')
     print(f'total of {count} {vlan}')
-#showvlan()
+# showvlan()
 
 def checkstatusdown():
     count = 0
     for item in re_all_list:
-        if item[0] == 'down':
+        if item[2] == 'down':
             count += 1
-            print(f'{item[0]} IP : {item[1]} , status : {item[2]}')
+            print(f'{item[0]} IP: {item[1]} Status: {item[2]}')
     print(f'total of {count} down')
 #checkstatusdown()
 
@@ -103,7 +105,6 @@ def checkvlanup():
     for item in re_all_list:
         if item[0] == vlan and item[2] == 'up':
             count += 1
-            print(f'{item[0]} IP : {item[1]} , status : {item[2]}')
+            print(f'{item[0]} IP: {item[1]} Status: {item[2]}')
     print(f'total of {count} {vlan} is up')
-#checkvlanup()
-#5
+# checkvlanup()
---
+++ June/main2.py
@@ -0,0 +1,33 @@
+from function2 import *
+def main():
+    while True:
+        print("\n📋 enter a number：")
+        print("1. show all information")
+        print("2. search from vlan")
+        print("3. check status down from global")
+        print("4. search from ip")
+        print("5. check status up from vlan")
+        print("6. check status from ip")
+        print("7. exit")
+        choice = input("input from（1-6）：").strip()
+
+        if choice == '1':
+            showallinfo()
+        elif choice == '2':
+            showvlan()
+        elif choice == '3':
+            checkstatusdown()
+        elif choice == '4':
+            checkip()
+        elif choice == '5':
+            checkvlanup()
+        elif choice == '6':
+            checkstatus()
+        elif choice == '7':
+            print("exit")
+            break
+        else:
+            print("invalid input")
+
+if __name__ == '__main__':
+    main()
