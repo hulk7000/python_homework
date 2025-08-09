031b136358b660409478996f61f02b1f3c026b7f
Hulk Zhang <hulk7000@proton.me>
2025-08-08 22:01:05 -0700

    trangle

---
+++ AUG/0808homwork/trangle.py
@@ -0,0 +1,15 @@
+row = 6
+
+# for i in range(row):
+#     for j in range(row - i - 1):
+#         print(' ', end='')
+#     for j in range(2 * i + 1):
+#         print('*', end='')
+#     print()
+
+for i in range(row):
+    for j in range(row - i - 1):
+        print(' ', end='')
+    for j in range(2 * i + 1):
+        print('*', end='')
+    print()
