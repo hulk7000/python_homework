9df9ea212c09e813c4685563d7b61a452c397c33
Hulk Zhang <hulk7000@proton.me>
2025-08-08 17:24:36 -0700

    new

--- AUG/0808homwork/homework.py
+++ AUG/0808homwork/homework.py
@@ -1,16 +1,16 @@
-from lib.dic import employee_data, erp_data, game_data, school_data, logistics_data
+from dic import *
 
 def q1():
     a = employee_data.get('employees')[0].get('projects')[1].get('name')
     print(a)
-#3
+#5
 
 def q2():
     b = erp_data.get('orders')[0].get('products')[0]
     print(b)
     for k,v in b.items():
         print(k,v)
-#3
+#5
 
 def q3():
     armor = game_data.get('equipment').get('armor')
@@ -20,22 +20,21 @@ def q3():
         print(f"当前值   {v.get('def')}")
         armor_total=armor_total+v.get('def')
         print(f"加之后的总和   {armor_total}")
-#3
+#5
 
 def q4():
-    students=school_data.get('students')
+    students = school_data.get('student')
     for student in students:
-        name=student.get('name')
-        scores=student.get('scores')
-        scores_total=0
+        name = student.get('name')
+        scores = student.get('scores')
+        scores_total = 0
         for k,v in scores.items():
-            scores_total=scores_total+v
-        print(f"name: {name} , scores: {scores_total}")
-#3
+            scores_total = scores_total+v
+        print(f'name: {name}, scores: {scores_total}')
+#5
 
 def q5():
     a = logistics_data.get('cargo')[0]
     for a in logistics_data.get('cargo'):
         print(f'item: {a.get('item')} qty: {a.get('qty')}')
-
-q5()
+#5
