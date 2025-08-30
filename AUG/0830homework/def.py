0f1cc322de2f997eecfc465a8c1eb5b2ef04d172
Hulk Zhang <hulk7000@proton.me>
2025-08-30 10:17:42 -0700

    new

---
+++ AUG/0830homework/def.py
@@ -0,0 +1,38 @@
+# def times(x,y):
+#     return x*y
+#
+# def ptimes(x,y):
+#     print(x+y)
+#
+# result = times(1,2)
+# print(result)
+# result = ptimes(1,2)
+# print(result)
+
+# def controltype(count: int, ws: str) -> dict:
+#     return {count: count * ws}
+#
+# print(controltype(2, 'test'))
+
+# print(controltype('sdf', 2))
+
+# l1 = [1,2,3,4,5]
+# for i in l1:
+#     print(i,end=' ')
+
+# res = []
+# for i in 'hello':
+#     res.append(i*4)
+# print(res)
+# rest = [c*4 for c in 'hello']
+# print(rest)
+
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print(matrix[1][1])
+
+# l1 = [1,2,3,4,5,6,7,8,9]
+# l1.insert(1,'p')
+# l1.remove('p')
+# l1.append(10)
+# l1.pop()
+# print(l1)
