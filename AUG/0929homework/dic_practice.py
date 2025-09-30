26dbae1d7bb99d3a738417753f89154004fe38b4
Hulk Zhang <hulk7000@proton.me>
2025-09-29 17:07:51 -0700

    new

---
+++ AUG/0929homework/dic_practice.py
@@ -0,0 +1,32 @@
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+def rd(lst):
+    result = []
+    for i in lst:
+        if i not in result:
+            result.append(i)
+    return result
+print(rd(nums))
+
+origin_list = [5, 9, 3, 77, 2, 6, 99, 8, 21, 6, 4]
+
+def sortlst(lst):
+    total = 0
+    for i in range(len(lst)):
+        for j in range(0, len(lst) - i - 1):
+            if lst[j] > lst[j + 1]:
+                print(f'{lst[j]} , {lst[j+1]}', end=' ')
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                total += 1
+                print(f'changed to : {lst[j]}, {lst[j+1]}')
+    print(f'total times {total}')
+    return lst
+
+print(sortlst(origin_list))
+
+def reverse_sort(lst):
+    for i in range(len(lst)):
+        j = len(lst) - i - 1
+        print(lst[j])
+
+reverse_sort(origin_list)
