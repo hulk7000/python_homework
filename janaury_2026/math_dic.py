949dbc9220e42c26828af32f30cf0a504646f92e
Hulk Zhang <hulk7000@proton.me>
2026-01-25 16:30:24 -0800

    new math

--- janaury_2026/math_dic.py
+++ janaury_2026/math_dic.py
@@ -5,4 +5,21 @@ def math_avage(n):
     avg = total/n
     return avg
 
-print(math_avage(100))
+# print(math_avage(100))
+
+def math_even(n):
+    total = 0
+    for i in range(2, n + 1, 2):
+        total += i
+    return total
+
+# print(math_even(100))
+
+def math(n):
+    count = 0
+    for i in range(1,n+1):
+        if i%2 == 0 and i%3 == 0:
+            count+=1
+    return count
+
+print(math(100))
