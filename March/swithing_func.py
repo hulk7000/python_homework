d5fc543b95d6c33f9cf1a0396c2d447163383a34
Hulk Zhang <hulk7000@proton.me>
2026-04-26 21:02:53 -0700

    use pycharm commit for this commit

--- March/swithing_func.py
+++ March/swithing_func.py
@@ -16,4 +16,29 @@ def oppist(a, b):
     b = temp
     return "new Output:", a, b
 
-print(oppist(5,8))
+# print(oppist(5,8))
+
+def count_char(s):
+    dic1 = {}
+    for letter in s:
+        if letter not in dic1:
+            times = s.count(letter)
+            dic1[letter] = times
+    return dic1
+
+# print(count_char("asa"))
+
+
+def intersect(a, b):
+    result = []
+    for x in a:
+        if x in b:
+            result.append(x)
+
+    return result
+
+list1 = [2, 3, 4]
+list2 = [1, 2, 3]
+
+answer = intersect(list1, list2)
+print(answer)
