e9893cbf613e125f89df2806f2cb1d280f6241b6
Hulk Zhang <hulk7000@proton.me>
2026-07-05 13:28:44 -0700

    lin lili python

---
+++ 2026/7,5,2026.py
@@ -0,0 +1,41 @@
+import math
+
+# a = -1
+# b = -2
+# c = 3
+# print((-b + math.sqrt(b** 2 - 4 * a * c)) / (2*a))
+# print((-b - math.sqrt(b** 2 - 4 * a * c)) / (2*a))
+
+# s = "hello world!"
+# print(len(s))
+#
+# print(s[0])
+# print(s[len(s) -1])
+
+# mood_index = int(input("what is your mood?: "))
+# if mood_index >= 60:
+#     print("you can play")
+# else:
+#     print("you can't play")
+
+# rows = 5
+#
+# for i in range(rows):
+#     spaces = " " * (rows - i - 1)
+#     stars = "*" * (2 * i + 1)
+    # print(spaces + stars)
+
+# user_weight = float(input("please enter your weight in kg : "))
+# user_height = float(input("please enter your height in meters : "))
+#
+# user_bmi = user_weight/user_height**2
+# print(f"your bmi is : {user_bmi}")
+#
+# if user_bmi <= 18.5:
+#     print("your bmi is a little less")
+# elif 18.5 < user_bmi <= 25:
+#     print("your bmi is perfect")
+# elif 25 < user_bmi <= 30:
+#     print("your bmi is a little more")
+# else:
+#     print("your bmi is very big")
